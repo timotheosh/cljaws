27f345133c9e0c64e17ba30e79a875390ab8099c
Tim Hawes <tim.hawes@genesys.com>
2024-06-03 15:24:32 -0400

    Use specific Corretto JDK

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject timotheosh/cljaws "0.4.1-SNAPSHOT"
+(defproject timotheosh/cljaws "0.4.2-SNAPSHOT"
   :description "Convenience functions for interacting with AWS API's from closh"
   :url "https://github.com/timotheosh/cljaws"
   :license {:name "MIT"
